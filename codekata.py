1bbfdff1249c88bca44519280697002d1b910dbc
Khushwant-Singh1 <khushwantzx@gmail.com>
2024-06-03 05:49:41 +0530

    codes

---
+++ codekata.py
@@ -0,0 +1,21 @@
+# class Calculation:
+#     def __init__(self,x,y):
+#         self.x = x
+#         self.y = y
+#
+#     def find_iteration(self):
+#         if self.x>self.y:
+#             return 1
+#         years = 0
+#         while self.x<=self.y:
+#             self.x*=3
+#             self.y*=4
+#             years+=1
+#             return years
+# if __name__ == "__main__":
+#     x,y = map(int, input().split())
+#     cal = Calculation(x,y)
+#     result = cal.find_iteration()
+#     if result is not None:
+#         print(result)
+
