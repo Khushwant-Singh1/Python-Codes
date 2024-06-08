166cad005d390dca82878158251492978789da9a
Khushwant-Singh1 <khushwantzx@gmail.com>
2024-06-09 01:24:59 +0530

    codes

--- filter.py
+++ filter.py
@@ -3,5 +3,5 @@ l =[12, 38, 9, 33, 5, 42, 47, 41, 43, 29]
 def filter_func(n):
     return n%2 ==0
 
-newl = list(filter(filter_func,l))
+newl = list(filter(lambda x: x%2==0,l)) # takes two arguments predicate and iterable
 print(newl)
